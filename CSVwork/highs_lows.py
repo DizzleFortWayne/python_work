72857a0107b524fd41fbffa089e5083fcbe78d25
PyBot <mcjstn@icloud.com>
2017-10-30 22:43:03 -0400

    Title string, Error checking
    Changed title to be based on string, added error checking and formatted title

--- CSVwork/highs_lows.py
+++ CSVwork/highs_lows.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 #get dates and high temp from file
-filename='sitka_weather_2014.csv'
+filename='death_valley_2014.csv'
 with open(filename) as f:
 	reader=csv.reader(f)
 	header_row=next(reader)
@@ -12,15 +12,16 @@ with open(filename) as f:
 	dates, highs, lows =[],[],[]
 	
 	for row in reader:
-		
-		current_date=datetime.strptime(row[0],"%Y-%m-%d")
-		dates.append(current_date)
-		
-		high=int(row[1])
-		highs.append(high)
-		
-		low=int(row[3])
-		lows.append(low)
+		try:
+			current_date=datetime.strptime(row[0],"%Y-%m-%d")
+			high=int(row[1])
+			low=int(row[3])
+		except ValueError:
+			print(current_date, 'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
 	
 #plot data
 fig= plt.figure(dpi=128, figsize=(10,6))
@@ -28,7 +29,8 @@ plt.plot(dates, highs, c='red', alpha=0.5)
 plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates,highs,lows, facecolor = 'blue',alpha=0.1)
 #format plot
-plt.title('Daily High and Low Temperatures- 2014',fontsize=24)
+title="Daily High and Low Temperatures- 2014\nDeath Valley,CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
